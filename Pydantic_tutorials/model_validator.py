b898eb79aa8f9cf09e78aa6f39aee2eb5cb6792b
Ashwini Kakde <kakdeashwini12@gmail.com>
2025-05-31 16:14:11 +0530

    Pydantic tutorials

---
+++ Pydantic_tutorials/model_validator.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr, model_validator
+from typing import List, Dict
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+    @model_validator(mode='after')
+    def validate_emergency_contact(cls, model):
+        if model.age > 60 and 'emergency' not in model.contact_details:
+            raise ValueError('Patients older than 60 must have an emergency contact')
+        return model
+
+
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
+
+patient_info = {'name':'Ashwini', 'email':'abc@icici.com', 'age': '65', 'weight': 56.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462', 'emergency':'235236'}}
+
+patient1 = Patient(**patient_info) 
+
+update_patient_data(patient1)
